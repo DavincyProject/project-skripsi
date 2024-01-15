411d660bd796a46cd3af666f60fc51b33f81fe8a
DavincyProject <bdavincy.bd@gmail.com>
2024-01-15 11:23:09 +0700

    fix: fix bugs when localstorage is undefined

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,15 +2,18 @@
 
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const Page = () => {
   const router = useRouter();
 
-  const checkIsLoggedIn = localStorage.getItem("isLoggedIn");
+  useEffect(() => {
+    const checkIsLoggedIn = localStorage.getItem("isLoggedIn");
 
-  if (checkIsLoggedIn) {
-    router.push("/");
-  }
+    if (checkIsLoggedIn) {
+      router.push("/");
+    }
+  }, []);
 
   const login = () => {
     localStorage.setItem("isLoggedIn", "true");
