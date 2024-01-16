57f739fda9cb17e1fdb44c6a3397927ec4601dc1
DavincyProject <bdavincy.bd@gmail.com>
2024-01-16 13:33:48 +0700

    feat: add new button to login pages

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -3,6 +3,8 @@
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import { RiLoginBoxFill } from "react-icons/ri";
+import { FaMoneyCheck } from "react-icons/fa6";
 
 const Page = () => {
   const router = useRouter();
@@ -53,8 +55,15 @@ const Page = () => {
     <>
       <div className="px-2">
         <div id="background-login"></div>
-        <div className="flex flex-wrap justify-around items-center min-h-screen">
-          <div className="max-w-[50%] hidden md:block">Background here</div>
+        <div className="flex flex-wrap justify-evenly items-center min-h-screen">
+          <div className="max-w-[50%] hidden md:block">
+            <Image
+              src={"/auth/ilus.svg"}
+              width={600}
+              height={600}
+              alt="logo login"
+            />
+          </div>
 
           <div className="md:max-w-[50%]">
             <div className="card shadow-xl border">
@@ -64,11 +73,8 @@ const Page = () => {
                   Gunakan Nomor Induk Mahasiswa (NIM) dan password yang <br />{" "}
                   berupa tanggal lahir Anda untuk mengakses Sisfo Akademik ini.
                 </small>
-                <small className="font-bold text-red-500">
-                  TESTING ONLY, NOT FINAL UI
-                </small>
 
-                <form className="w-full">
+                <form className="w-full text-right">
                   <label className="w-full form-control">
                     <div className="label">
                       <span className="label-text">NIM</span>
@@ -90,23 +96,42 @@ const Page = () => {
                     />
                   </label>
                 </form>
+                <div className="mt-4 flex justify-between items-center flex-wrap gap-5 w-full">
+                  <div className="form-control border flex-1 p-1">
+                    <label className="label cursor-pointer">
+                      <div className="flex justify-between items-center w-full">
+                        <div className="flex items-center gap-3">
+                          <input type="checkbox" className="checkbox " />
+                          <span className="label-text">
+                            I&apos;m not a robot
+                          </span>
+                        </div>
+                        <Image
+                          src={"/auth/recaptcha.png"}
+                          width={35}
+                          height={35}
+                          alt="google recaptcha"
+                        />
+                      </div>
+                    </label>
+                  </div>
 
-                <div className="card-actions">
                   <button
                     onClick={login}
-                    className="btn btn-primary btn-wide text-white"
+                    className="btn btn-primary hover:bg-darkblue btn-wide text-white font-bold text-md"
                   >
                     Masuk
+                    <RiLoginBoxFill size={18} />
                   </button>
                 </div>
 
                 <div className="divider">atau</div>
 
                 <div className="flex justify-center flex-wrap gap-2">
-                  <button className="btn btn-primary btn-wide text-white">
+                  <button className="btn btn-info hover:bg-darkblue btn-wide text-white ">
                     Aktivasi Mahasiswa Keluar
                   </button>
-                  <button className="btn bg-[#ff3300] hover:bg-[#b64529] btn-wide text-white">
+                  <button className="btn btn-info hover:bg-darkblue btn-wide text-white">
                     Cek NIM Baru / Surat Keluar / Surat DO
                   </button>
                 </div>
