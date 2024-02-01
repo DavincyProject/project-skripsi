42f23cbb3b1ac2c7679a018914148aea264b0148
DavincyProject <bdavincy.bd@gmail.com>
2024-02-01 15:15:04 +0700

    feat: add print function to krs

---
+++ src/app/testing/page.tsx
@@ -0,0 +1,12 @@
+import PrintLayout from "@/components/printLayout/PrintLayout";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="min-h-dvh py-2">
+      <PrintLayout />
+    </div>
+  );
+};
+
+export default page;
