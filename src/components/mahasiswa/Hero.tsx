23e4acb4e853b6802af9f5f94b283ec7a22a8e04
Billy <76769657+DavincyProject@users.noreply.github.com>
2024-12-30 20:53:23 +0700

    Update Hero.tsx

--- src/components/mahasiswa/Hero.tsx
+++ src/components/mahasiswa/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
           <div className="flex items-end justify-center">
             <Image
               src={
-                "https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                "/picture_profile.png"
               }
               alt="profile image"
               loading="lazy"
